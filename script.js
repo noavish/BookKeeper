201396daa04d12895f10fc923e6858e9a64635c3
noavish <noavish1@gmail.com>
2017-12-25 10:12:03 +0200

    final version

--- script.js
+++ script.js
@@ -24,18 +24,20 @@ function inputValidation (parentElement, text, isValidFunction) {
 }
 
 function submitForm () {
-    if (inputValidation('rUserName', 'The supplied username is too short, try again using 5 or more characters', function (valueString) {return valueString.length >= 5;}) &&
-    inputValidation('rPassword', 'The password needs at least 6 characters', function (valueString) {return valueString.length >= 6;}) &&
-    inputValidation('rPassword2', 'Passwords have to be the same', function(valueString) {return valueString === document.getElementById('password').value;}) &&
-    inputValidation('rCountry', 'You can only sign-up from Israel or from USA', function (valueString) {return (valueString === "United States" || valueString === "Israel");}) &&
-    inputValidation('rPhoneNumber', 'Phone number must be shorter than 15 digits', function (valueString) {return valueString.length < 15;})) {
+    var isAllValid = inputValidation('rUserName', 'The supplied username is too short, try again using 5 or more characters', function (valueString) {return valueString.length >= 5;});
+    isAllValid =  inputValidation('rPassword', 'The password needs at least 6 characters', function (valueString) {return valueString.length >= 6;}) && isAllValid;
+    isAllValid =  inputValidation('rPassword2', 'Passwords have to be the same', function(valueString) {return valueString === document.getElementById('password').value;}) && isAllValid;
+    isAllValid = inputValidation('rPhoneNumber', 'Phone number must be shorter than 15 digits', function (valueString) {return valueString.length < 15;}) && isAllValid;
+    isAllValid = inputValidation('rCountry', 'You can only sign-up from Israel or from USA', function (valueString) {return (valueString === "United States" || valueString === "Israel");}) && isAllValid;
+    
+    if (isAllValid) {   
         var greeting = document.createElement('h2');
         var greetingMessage = document.createTextNode(`Welcome ${document.getElementById('userName').value}`);
         greeting.appendChild(greetingMessage);
         greeting.style.textAlign = 'center';
         greeting.style.fontFamily = 'Verdana';
         greeting.style.color = 'green';
-        document.getElementById('form').appendChild(greeting);
+        document.getElementById('welcome').appendChild(greeting);
     }    
 }
 
